ddab457f68a3b61a476cfd403e71bb31f16cfaa9
TRAMPlin30 <vicodin28@gmail.com>
2022-01-15 17:20:28 +0200

    SequenceSquares finished completely

--- src/com/ElementaryTasks/SequenceSquares/Main.java
+++ src/com/ElementaryTasks/SequenceSquares/Main.java
@@ -1,18 +1,21 @@
 package com.ElementaryTasks.SequenceSquares;
 
 public class Main {
+
+    public static int parameter;
+
     public static void main(String[] args) {
-        // 7. Числовая последовательность
-        sequence(27);
 
+        UserInteraction.printInstruction();
+        System.out.println("Enter parameter: ");
+        parameter = UserInteraction.enterParameters();
+
+        sequence(parameter);
     }
-    public static void sequence(int n) {
-
-        double sqrt_from_n = Math.sqrt(n);
+    public static void sequence(int parameter) {
+        double sqrt_from_n = Math.sqrt(parameter);
         int sqr = (int) sqrt_from_n;
-
         for (int i=1; i <= sqr; i+=1) {
-
             System.out.printf("%s, ", i);
         }
     }
