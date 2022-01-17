4f00796a66f0d9be4f42dc1318b3540a2ffeed4f
TRAMPlin30 <vicodin28@gmail.com>
2022-01-17 11:09:12 +0200

    Sort TriangleSet (with sorting)

--- src/com/ElementaryTasks/SortingTrianglesSet/TriangleSet.java
+++ src/com/ElementaryTasks/SortingTrianglesSet/TriangleSet.java
@@ -11,6 +11,11 @@ public class TriangleSet implements Comparable<TriangleSet> {
     private float square;
 
     public TriangleSet(String name, float sideA, float sideB, float sideC) {
+        /**
+         * Треугольник существует только тогда, когда сумма двух его сторон больше третьей.
+         * ... Если хотя бы в одном случае сторона окажется больше либо равна сумме двух других,
+         * то треугольника с такими сторонами не существует.
+         */
 
         if ((sideA + sideB)> sideC && (sideB + sideC)> sideA && (sideC + sideA)> sideB) {
             this.name = name;
